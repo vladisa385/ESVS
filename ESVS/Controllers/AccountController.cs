09a6e8262e4a837eab1fe3aa3ca05761b9b4d983
MrMagic24 <kirillyashenkov@gmail.com>
2018-10-16 16:36:02 +0700

    Halo Pachany

--- ESVS/Controllers/AccountController.cs
+++ ESVS/Controllers/AccountController.cs
@@ -11,13 +11,33 @@ namespace ESVS.Controllers
     [Route("api/[controller]")]
     public class AccountController : Controller
     {
+        //200 - OK
+        //400 - 
+        // CreateRole like a Register +
+        // GetListRoles like a GetList +
+        // GetRoles/{roleId} like a Get/{userId} +
+        // DeleteRole/{roleId} like a Delete/{userId} +
+        // UpdateRole like a UpdateUser +
+        // AddRoleToUser/{roleId} & /{userId}
+        // DeleteRoleToUser/{roleId} & /{userId}
+
         [HttpGet("GetList")]
         [Authorize]
         [ProducesResponseType(401)]
         [ProducesResponseType(200, Type = typeof(ListResponse<UserResponse>))]
         public async Task<IActionResult> GetUsersListAsync(UserFilter user, ListOptions options, [FromServices]IUsersListQuery query)
         {
-            var response = await query.RunAsync(user, options);
+            var response = await query.RunAsync(user, options);     // запрос к базе 
+            return Ok(response);
+        }
+
+        [HttpGet("GetListRoles")]
+        [Authorize]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(200, Type = typeof(ListResponse<UserResponse>))]
+        public async Task<IActionResult> GetRoleListAsync(RoleFilter role, ListOptions options, [FromServices]IRolesListQuery query)
+        {
+            var response = await query.RunAsync(role, options);     // запрос к базе 
             return Ok(response);
         }
 
@@ -31,6 +51,18 @@ namespace ESVS.Controllers
             UserResponse response = await query.RunAsync(userId);
             return response == null ? (IActionResult)NotFound() : Ok(response);
         }
+
+        [HttpGet("GetRole/{roleId}", Name = "GetSingleRole")]
+        [Authorize]
+        [ProducesResponseType(200, Type = typeof(RoleResponse))]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetRoleAsync(Guid roleId, [FromServices] IRoleQuery query)
+        {
+            RoleResponse response = await query.RunAsync(roleId);
+            return response == null ? (IActionResult)NotFound() : Ok(response);
+        }
+
         [HttpPost("Register")]
         [ProducesResponseType(201, Type = typeof(UserResponse))]
         [ProducesResponseType(400)]
@@ -82,6 +114,32 @@ namespace ESVS.Controllers
 
         }
 
+        [HttpPost("UpdateRole")]
+        [ProducesResponseType(201, Type = typeof(UserResponse))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(401)]
+        [Authorize]
+        public async Task<IActionResult> UpdateRole(UpdateRoleRequest role, [FromServices] IUpdateRoleCommand command)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                UserResponse response = await command.ExecuteAsync(role);
+                return CreatedAtRoute("GetSingleRole", new { roleId = response.Id }, response);
+
+            }
+            catch (CannotCreateRoleExeption exception)
+            {
+                foreach (var error in exception.Errors)
+                {
+                    ModelState.AddModelError(exception.Message, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+
+        }
+
         [HttpPost("ChangeUserPassword")]
         [ProducesResponseType(201, Type = typeof(UserResponse))]
         [ProducesResponseType(400)]
@@ -107,6 +165,7 @@ namespace ESVS.Controllers
             }
 
         }
+
         [HttpPost("Login")]
         [ProducesResponseType(200, Type = typeof(UserResponse))]
         [ProducesResponseType(400)]
@@ -126,6 +185,28 @@ namespace ESVS.Controllers
 
         }
 
+        [HttpPost("CreateRole")]
+        [ProducesResponseType(201, Type = typeof(RoleResponse))]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> CreateRole(CreateRoleRequest role, [FromServices] ICreateRoleCommand command)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            try
+            {
+                RoleResponse response = await command.ExecuteAsync(role);
+                return CreatedAtRoute("GetSingleRole", new { roleId = response.Id }, response);
+
+            }
+            catch (CannotCreateRoleExeption exception)
+            {
+                foreach (var error in exception.Errors)
+                {
+                    ModelState.AddModelError(exception.Message, error.Description);
+                }
+                return BadRequest(ModelState);
+            }
+        }
 
         [HttpPost("LogOff")]
         [ProducesResponseType(200)]
@@ -148,6 +229,25 @@ namespace ESVS.Controllers
             try
             {
                 await command.ExecuteAsync(userId);
+                return NoContent();
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [Authorize(Roles = "admin")]
+        [HttpDelete("DeleteRole/{roleId}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(401)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> DeleteRoleAsync(Guid roleId, [FromServices]IDeleteRoleCommand command)
+        {
+            try
+            {
+                await command.ExecuteAsync(roleId);
                 return NoContent();
             }
             catch (Exception exception)
