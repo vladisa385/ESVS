ac3f66061862c9457ac97144df8da77e76d2ba9d
Виталий Доманцевич <doman.d@yandex.ru>
2018-10-16 16:30:45 +0700

    Papich ne vinovat

---
+++ DataAccess/Users/CannotCreateRoleExeption.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataAccess.Users
+{
+    public class CannotCreateRoleExeption : Exception
+    {
+        public IEnumerable<IdentityError> Errors { get; set; }
+        public CannotCreateRoleExeption(IEnumerable<IdentityError> errors) : base("Role cannot be created")
+        {
+            Errors = errors;
+        }
+    }
+}
---
+++ ViewModel/Users/RoleFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ViewModel.Users
+{
+    public class RoleFilter
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
