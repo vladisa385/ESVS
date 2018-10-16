df681786f89015f7ce46334baae743878b343be8
Виталий Доманцевич <doman.d@yandex.ru>
2018-10-16 17:34:27 +0700

    2

---
+++ DB/Migrations/20181016101150_2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DB.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoleDescription",
+                table: "AspNetRoles",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleDescription",
+                table: "AspNetRoles");
+        }
+    }
+}
