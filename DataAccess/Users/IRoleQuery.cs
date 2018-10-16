491829fc3bae1755a825432b42105017c93a6cbe
Виталий Доманцевич <doman.d@yandex.ru>
2018-10-16 16:23:55 +0700

    урааа

---
+++ DataAccess/Users/IRoleQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel.Users;
+
+namespace DataAccess.Users
+{
+    public interface IRoleQuery
+    {
+        Task<RoleResponse> RunAsync(Guid roleId);
+    }
+}
---
+++ DataAccess/Users/IRolesListQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel;
+using ViewModel.Users;
+
+namespace DataAccess.Users
+{
+    public interface IRolesListQuery
+    {
+        Task<ListResponse<RoleResponse>> RunAsync(RoleFilter filter, ListOptions options);
+
+    }
+}
