f02e18a841755b26ed92252b1b0014733339eb5b
Виталий Доманцевич <doman.d@yandex.ru>
2018-10-23 16:20:44 +0700

    удаляет и создает роли,проверяйте

--- DataAccess.DbImplementation/Users/CreateRoleCommand.cs
+++ DataAccess.DbImplementation/Users/CreateRoleCommand.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 using AutoMapper;
 using DataAccess.Users;
-using DB;
 using Entities;
 using Microsoft.AspNetCore.Identity;
 using ViewModel.Users;
@@ -11,19 +13,14 @@ namespace DataAccess.DbImplementation.Users
     public class CreateRoleCommand : ICreateRoleCommand
     {
         private readonly RoleManager<Role> _roleManager;
-        private readonly SignInManager<Role> _signInManager;
         private readonly IMapper _mapper;
-        private readonly AppDbContext _context;
 
         public CreateRoleCommand(
             RoleManager<Role> roleManager,
-            SignInManager<Role> signInManager,
-            IMapper mapper, AppDbContext context)
+            IMapper mapper)
         {
             _roleManager = roleManager;
-            _signInManager = signInManager;
             _mapper = mapper;
-            _context = context;
         }
         public async Task<RoleResponse> ExecuteAsync(CreateRoleRequest request)
         {
@@ -41,8 +38,6 @@ namespace DataAccess.DbImplementation.Users
             // установка куки
             if (!result.Succeeded)
                 throw new CannotCreateUserExeption(result.Errors);
-
-            await _signInManager.SignInAsync(role, false);
 
             return _mapper.Map<Role, RoleResponse>(role);
 
--- ViewModel/Users/CreateRoleRequest.cs
+++ ViewModel/Users/CreateRoleRequest.cs
@@ -6,7 +6,6 @@ namespace ViewModel.Users
     {
         [Required]
         [Display(Name = "Name")]
-        [EmailAddress]
         public string Name { get; set; }
 
         [Required]
