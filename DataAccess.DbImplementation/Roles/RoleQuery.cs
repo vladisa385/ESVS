1325806eacee7dc554b67f2a741182a88a4d54b7
Виталий Доманцевич <doman.d@yandex.ru>
2018-11-06 14:57:39 +0700

    RoleQuery + RoleListQuery

---
+++ DataAccess.DbImplementation/Roles/RoleQuery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper.QueryableExtensions;
+using DataAccess.Roles;
+using Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ViewModel.Roles;
+
+namespace DataAccess.DbImplementation.Roles
+{
+    public class RoleQuery : IRoleQuery
+    {
+        private readonly RoleManager<Role> _roleManager;
+
+        public RoleQuery(RoleManager<Role> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public async Task<RoleResponse> RunAsync(Guid roleId)
+        {
+
+            RoleResponse response = await _roleManager.Roles.Include("Recipies")
+                .Include("Recipies")
+                .Include("Reviews")
+                .Include("CheapPlaces")
+                .Include("RateReviews")
+                .Include("RateCheapPlaces")
+                .ProjectTo<RoleResponse>()
+                .FirstOrDefaultAsync(p => p.Id == roleId);
+            return response;
+        }
+    }
+}
--- DataAccess.DbImplementation/Roles/UpdateRoleCommand.cs
+++ DataAccess.DbImplementation/Roles/UpdateRoleCommand.cs
@@ -1,6 +1,45 @@
-﻿namespace DataAccess.DbImplementation.Roles
+﻿/*using System.Threading.Tasks;
+using AutoMapper;
+using DataAccess.Roles;
+using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using ViewModel.Roles;
+
+namespace DataAccess.DbImplementation.Roles
 {
-    class UpdateRoleCommand
+    public class UpdateRoleCommand : IUpdateRoleCommand
     {
+        private readonly RoleManager<Role> _roleManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMapper _mapper;
+        private readonly IHostingEnvironment _appEnvironment;
+        public UpdateRoleCommand(RoleManager<Role> roleManager,
+            IMapper mapper,
+            IHttpContextAccessor httpContextAccessor,
+            IHostingEnvironment appEnvironment)
+        {
+            _roleManager = roleManager;
+            _mapper = mapper;
+            _httpContextAccessor = httpContextAccessor;
+
+            _appEnvironment = appEnvironment;
+        }
+        public async Task<RoleResponse> ExecuteAsync(UpdateRoleRequest request)
+        {
+
+
+            var foundRole = await _roleManager.GetRoleIdAsync(_httpContextAccessor.HttpContext.Role);
+
+
+            foundRole.Name = request.Name;
+            foundRole.RoleDescription = request.RoleDescription;
+
+            await _roleManager.UpdateAsync(foundRole);
+
+            return _mapper.Map<Role, RoleResponse>(foundRole);
+
+        }
     }
-}
+}*/
