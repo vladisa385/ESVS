70567489534a88bd9b3fa11df6b8e26f74c6b097
vladisa385 <vladisa375@gmail.com>
2019-01-03 16:14:37 +0700

    test

---
+++ AppForMigrationDataFromKmiacDB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppForMigrationDataFromKmiacDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
