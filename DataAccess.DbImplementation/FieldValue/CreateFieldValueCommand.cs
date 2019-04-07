50d6ce44308eed8c7b1d1b69000292bf3af19b25
vladisa385 <vladisa375@gmail.com>
2019-04-07 18:20:39 +0700

    исправляем косяки Витали

--- DataAccess.DbImplementation/FieldValue/CreateFieldValueCommand.cs
+++ DataAccess.DbImplementation/FieldValue/CreateFieldValueCommand.cs
@@ -1,7 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using AutoMapper;
 using DB;
 using DataAccess.FieldValue;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using ViewModel.FieldValues;
 
 namespace DataAccess.DbImplementation.FieldValue
@@ -18,6 +22,16 @@ namespace DataAccess.DbImplementation.FieldValue
         }
         public async Task<FieldValuesResponse> ExecuteAsync(CreateFieldValuesRequest request)
         {
+            if (!await _context.Fields.AnyAsync(u => u.Id == request.FieldId))
+                throw new CannotCreateFieldValuesException(
+                    new List<IdentityError>()
+                    {
+                        new IdentityError()
+                        {
+                            Description = "Field with with Id doesnt exist"
+                        }
+                    }
+                    );
             var fieldvalues = _mapper.Map<CreateFieldValuesRequest, Entities.FieldValue>(request);
             await _context.AddAsync(fieldvalues);
             await _context.SaveChangesAsync();
